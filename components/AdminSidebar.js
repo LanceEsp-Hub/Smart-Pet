ef683b2cd214222255ec5ef721cce3f8ac3e396d
christj831 <sijhayortega12@gmail.com>
2025-07-26 18:06:42 +0800

    Update AdminSidebar.js

--- components/AdminSidebar.js
+++ components/AdminSidebar.js
@@ -133,8 +133,23 @@ const AdminSidebar = () => {
           />
         </svg>
       ),
-      label: "Forms",
+      label: "Pet Tracker Management",
       description: "Application Forms",
+    },
+        {
+      path: "/admin/pet_tracker_management",
+      icon: (
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+          />
+        </svg>
+      ),
+      label: "Forms",
+      description: "Pet Tracker Management",
     },
   ]
 
