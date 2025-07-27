4a56ea50cb0e5450a4fa066170127f6c29fda80d
Lance Cedrick F. Espina <lancecedrickespina@gmail.com>
2025-07-27 17:50:55 +0800

    Update account_information.js

--- pages/settings/account_information.js
+++ pages/settings/account_information.js
@@ -442,7 +442,7 @@ const AccountInformationPage = () => {
                           name="name"
                           value={formData.name}
                           onChange={handleChange}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                          className="w-full px-3 py-2 border text-black border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                         />
                       </div>
 
