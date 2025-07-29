6579e36ccfa3da174718ffdf11aa950b196a9db4
Lance Cedrick F. Espina <lancecedrickespina@gmail.com>
2025-07-29 13:22:55 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,37 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   eslint: {
+//     ignoreDuringBuilds: true, // Disables all ESLint errors during build
+//   },
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true, // Disables all ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['cdnjs.cloudflare.com'], // For Leaflet marker icons
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+      ],
+    });
+    return config;
   },
 };
 
