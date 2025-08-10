489d3e10d468a6acbaeed4108737f25160a727ea
christj831 <sijhayortega12@gmail.com>
2025-08-11 02:51:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disables all ESLint errors during build
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'newback-production-a0cc.up.railway.app',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
